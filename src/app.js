e50fbc6d90a2bd45888445746dde37198898aaab
Abhishek Choudhary <acabhishek942@gmail.com>
2018-05-07 03:42:19 +0530

    Data binding

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 new Vue({
   el:'#view-app',
   data:{
-    name:'Abhishe'
+    name:'Abhishe',
+    job:'Sucks',
+    website:'http://www.dummy.com',
+    websiteTag:'<a href="http://www.dummy.com">My Website</a>'
+  },
+  methods:{
+    greet: function(time){
+      return 'Good ' + time + ' ' + this.name;
+    }
   }
 });
