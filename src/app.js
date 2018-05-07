12a217d60d808877fcacad836bc1140d4284d178
Abhishek Choudhary <acabhishek942@gmail.com>
2018-05-08 00:30:41 +0530

    Dynamic classes

--- src/app.js
+++ src/app.js
@@ -1,27 +1,19 @@
 new Vue({
   el:'#view-app',
   data:{
-    age: 20,
-    a: 0,
-    b: 0,
-
+    available: false,
+    nearby: false,
   },
   methods:{
-    // addToA:function() {
-    //   return this.a + this.age;
-    // },
-    // addToB:function() {
-    //   return this.b + this.age;
-    // },
+
   },
   computed:{
-    addToA:function() {
-      console.log("Afunc");
-      return this.a + this.age;
-    },
-    addToB:function() {
-      console.log("Bfunc");
-      return this.b + this.age;
-    },
+    compClasses:function() {
+      return {
+        available: this.available,
+        nearby: this.nearby
+      }
+
+    }
   }
 });
