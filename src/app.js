7353300d41b9b0d97e1ebf39324b0529ce23eb1e
Abhishek Choudhary <acabhishek942@gmail.com>
2018-05-07 23:01:02 +0530

    Keyboard events

--- src/app.js
+++ src/app.js
@@ -1,21 +1,15 @@
 new Vue({
   el:'#view-app',
   data:{
-    age:25,
-    x:0,
-    y:0
+
   },
   methods:{
-    add:function(increment) {
-      this.age += increment;
+    logName:function() {
+      console.log("you entered name");
     },
-    substract:function(decrement) {
-      this.age -= decrement;
-    },
-    updateXY:function(event){
-      this.x = event.offsetX;
-      this.y = event.offsetY;
 
+    logAge:function() {
+      console.log("You entered age");
     }
 
     }
