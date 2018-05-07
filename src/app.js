0a8f7bb671443f77bc939e0eedefd8e0de5e805a
Abhishek Choudhary <acabhishek942@gmail.com>
2018-05-07 23:38:14 +0530

    Computed properties

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
 new Vue({
   el:'#view-app',
   data:{
-    name: '',
-    age: ''
+    age: 20,
+    a: 0,
+    b: 0,
 
   },
   methods:{
-    logName:function() {
-      console.log("you entered name");
+    // addToA:function() {
+    //   return this.a + this.age;
+    // },
+    // addToB:function() {
+    //   return this.b + this.age;
+    // },
+  },
+  computed:{
+    addToA:function() {
+      console.log("Afunc");
+      return this.a + this.age;
     },
-
-    logAge:function() {
-      console.log("You entered age");
-    }
-
-    }
+    addToB:function() {
+      console.log("Bfunc");
+      return this.b + this.age;
+    },
+  }
 });
