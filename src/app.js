20e029fda046d4b3bf1e31c9d9eac160186cc2fb
Abhishek Choudhary <acabhishek942@gmail.com>
2018-05-08 22:51:11 +0530

    Vue Refs

--- src/app.js
+++ src/app.js
@@ -1,50 +1,15 @@
-Vue.component('greeting', {
-  template: "<p> Reusable template {{ name }} <button v-on:click='changeName()'>Change Name</button></p>",
-  data:function(){
-    return {
-      name: 'Yoshi'
-    }
-  },
-  methods: {
-    changeName: function(){
-      this.name = "Mordoe";
-    }
-  }
-})
 
 var one = new Vue({
   el:'#view-app-one',
   data:{
-    title: 'App-1',
-
+    output: 'Bacon',
   },
   methods:{
-
+    readRefs:function(){
+      console.log(this.$refs.test.innerText);
+      this.output = this.$refs.inputRef.value;
+    }
   },
   computed:{
-    greet:function(){
-      return "Hello from App-1"
-    }
   }
 });
-
-
-var two = new Vue({
-  el:'#view-app-two',
-  data:{
-    title: 'App-2',
-  },
-  methods:{
-    changeTitle:function() {
-      one.title = "Changed from 2nd instance";
-    }
-
-  },
-  computed:{
-    greet:function() {
-      return "Hello from App-2"
-    }
-  }
-});
-
-two.title="Changed from outside";
