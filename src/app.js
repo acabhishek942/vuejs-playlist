53b23b8333a0ac578dd26223086b0a0963bbc80a
Abhishek Choudhary <acabhishek942@gmail.com>
2018-05-08 21:14:16 +0530

    multiple vue instances

--- src/app.js
+++ src/app.js
@@ -1,16 +1,36 @@
-new Vue({
-  el:'#view-app',
+var one = new Vue({
+  el:'#view-app-one',
   data:{
-    characters: ['Mario', 'Luigi', 'Yoshi', 'IronMan'],
-    ninjas: [
-      {name:'Ryu', age:25},
-      {name:'Yoshi', age:35},
-      {name:'Ken', age:55},
-    ]
+    title: 'App-1',
+
   },
   methods:{
 
   },
   computed:{
+    greet:function(){
+      return "Hello from App-1"
+    }
   }
 });
+
+
+var two = new Vue({
+  el:'#view-app-two',
+  data:{
+    title: 'App-2',
+  },
+  methods:{
+    changeTitle:function() {
+      one.title = "Changed from 2nd instance";
+    }
+
+  },
+  computed:{
+    greet:function() {
+      return "Hello from App-2"
+    }
+  }
+});
+
+two.title="Changed from outside";
