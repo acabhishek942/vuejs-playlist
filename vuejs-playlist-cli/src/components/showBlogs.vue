0fc6494d0cf132ce8b94305d5b413e408fe85958
Abhishek Choudhary <acabhishek942@gmail.com>
2018-05-13 22:22:49 +0530

    custom search filters using computed functions

--- vuejs-playlist-cli/src/components/showBlogs.vue
+++ vuejs-playlist-cli/src/components/showBlogs.vue
@@ -1,7 +1,8 @@
 <template>
     <div v-theme:column="'wide'" id="show-blogs">
         <h1>All Blog Articles</h1>
-        <div v-for="blog in blogs" class="single-blog">
+        <input type='text' v-model='search' placeholder='search blogs' />
+        <div v-for="blog in filterdBlogs" class="single-blog">
             <h2 v-rainbow>{{ blog.title | to-uppercase}}</h2>
             <article>{{ blog.body | snippet}}</article>
         </div>
@@ -15,6 +16,7 @@ export default {
   data() {
     return {
       blogs: [],
+      search: ''
     }
   },
   methods: {
@@ -23,6 +25,13 @@ export default {
       this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data){
           this.blogs = data.body.slice(0,10);
       });
+  },
+  computed: {
+    filterdBlogs:function(){
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search);
+      });
+    }
   }
 
 }
