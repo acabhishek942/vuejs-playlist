888238e71914a7680ab48af480b50ca1dae9f461
Abhishek Choudhary <acabhishek942@gmail.com>
2018-05-13 14:17:17 +0530

    started creating blog and used v-model data binding

--- vuejs-playlist-cli/src/App.vue
+++ vuejs-playlist-cli/src/App.vue
@@ -1,13 +1,17 @@
 <template>
+  <div>
+    <add-blog></add-blog>
+  </div>
+
 
 </template>
 
 <script>
-
+import addBlog from './components/addBlog.vue';
 
 export default {
   components:{
-
+    'add-blog': addBlog
   },
   data() {
     return {
@@ -20,4 +24,8 @@ export default {
 </script>
 
 <style>
+body{
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
 </style>
