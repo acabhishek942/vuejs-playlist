1b0339cb5e313b513b21cb71285f3d8519fcd2b8
Abhishek Choudhary <acabhishek942@gmail.com>
2018-05-13 13:54:12 +0530

    Deleted every unnecessary files

--- vuejs-playlist-cli/src/App.vue
+++ vuejs-playlist-cli/src/App.vue
@@ -1,25 +1,16 @@
 <template>
-  <div>
-    <keep-alive>
-      <component v-bind:is="components"></component>
-    </keep-alive>
-    <button v-on:click="components='form-one'">Show form one</button>
-    <button v-on:click="components='form-two'">Show form two</button>
-  </div>
+
 </template>
 
 <script>
-import formOne from './components/formOne.vue'
-import formTwo from './components/formTwo.vue'
+
 
 export default {
   components:{
-    'form-one': formOne,
-    'form-two': formTwo
+
   },
   data() {
     return {
-      components: 'form-two'
     }
   },
   methods: {
